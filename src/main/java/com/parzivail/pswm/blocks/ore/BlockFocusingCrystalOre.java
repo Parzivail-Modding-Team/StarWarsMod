8d2df6126c935762863b84f66dcd0294ee6cd133
kb1000 <kaeptmblaubaer1000@gmail.com>
2021-03-28 20:05:28 +0200

    Make focusing crystal ore drop the focusing crystal

--- src/main/java/com/parzivail/pswm/blocks/ore/BlockFocusingCrystalOre.java
+++ src/main/java/com/parzivail/pswm/blocks/ore/BlockFocusingCrystalOre.java
@@ -1,12 +1,16 @@
 package com.parzivail.pswm.blocks.ore;
 
 import com.parzivail.pswm.Resources;
+import com.parzivail.pswm.StarWarsItems;
 import com.parzivail.pswm.StarWarsMod;
 import com.parzivail.util.block.PBlock;
 import com.parzivail.util.world.HarvestLevel;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
 import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class BlockFocusingCrystalOre extends PBlock
 {
@@ -23,5 +27,11 @@ public class BlockFocusingCrystalOre extends PBlock
 	public boolean isReplaceableOreGen(World world, int x, int y, int z, Block target)
 	{
 		return true;
+	}
+
+	@Override
+	public Item getItemDropped(int p_149650_1_, Random p_149650_2_, int p_149650_3_)
+	{
+		return StarWarsItems.focusingCrystal;
 	}
 }
